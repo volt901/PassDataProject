1df4a192ef6573ac60ad7a2ffcd16f44b80bf5b9
vladimir <volt901@icloud.com>
2021-12-12 14:24:49 +0500

    Initial Commit

---
+++ PassDataProject/PassDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassDataProjectApp.swift
+//  PassDataProject
+//
+//  Created by Владимир Ширяев on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct PassDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
