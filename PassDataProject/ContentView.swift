16c901493205866050225bf6b2a0ab86b1bf926d
vladimir <volt901@icloud.com>
2021-12-14 23:15:11 +0500

    Update ContentView.swift

--- PassDataProject/ContentView.swift
+++ PassDataProject/ContentView.swift
@@ -6,11 +6,61 @@
 //
 
 import SwiftUI
+struct User {
+    let userName: String
+    let userPassword: String
+}
 
-struct ContentView: View {
+let secureUser = User(userName: "Vova", userPassword: "Vova")
+
+
+struct DetailView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello")
+    }
+}
+struct ContentView: View {
+    @State private var showingAlert = false
+    @State private var password = ""
+    @State private var userName = ""
+    @State var selection: Int? = nil
+    var body: some View {
+        NavigationView {
+            VStack{
+                VStack{
+                    SecureField("USERNAME", text: $userName)
+                    TextField("PASSWORD", text:$password)
+                }.textFieldStyle(.roundedBorder).padding()
+                
+                NavigationLink(destination:
+                                DetailView(), tag:1, selection: $selection) {
+                    Button(action: {
+                        print("login tapped")
+                        if password == secureUser.userPassword {
+                            self.selection = 1
+                        } else {
+                            self.selection = 999
+                            showingAlert = true
+                        }
+                    }) // :ACTION BUTTON
+                    {
+                        HStack {
+                            Spacer()
+                            Text("Login").foregroundColor(Color.white).bold()
+                            Spacer()
+                        }
+                    }//: TEXT BUTTON
+                    .accentColor(Color.black)
+                    .padding()
+                    .background(Color(UIColor.darkGray))
+                    .cornerRadius(4.0)
+                    .padding()
+                    .alert("Error", isPresented: $showingAlert) {
+                            Button("OK", role: .cancel) { }
+                        } //: ALER
+                }//:NavigationLink
+            }//:VSTACK
+        }//:NavigationView
     }
 }
 
@@ -19,3 +69,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
